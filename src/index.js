37b59671eb7a28df49fb03f60ac60e5e2608d7d2
Bisal Shah <bisalshah24@gmail.com>
2025-07-02 12:42:24 +0530

    compare project

--- src/index.js
+++ src/index.js
@@ -11,3 +11,44 @@ connectDB()
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
   });
+/*
+understand this
+Just for demo purposeAdd commentMore actions
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log("DB Connected");
+        app.on("error", (err) => {
+            console.error("ERROR: ", error)
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on PORT: ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw error
+    }
+})()
+*/
+
+/*
+understand this
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+*/
